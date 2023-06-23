493f1ce0391212a88fdd92043b4794696039f20a
Syed Faraz <syedfarazahmed.8@gmail.com>
2023-06-23 14:17:05 +0500

    map method mmultiple returns prob solved

--- scripts/array_methods.js
+++ scripts/array_methods.js
@@ -1,20 +1,26 @@
 //Important Array Methods
 const users = [
-    {userId : 1, firstName : "Qamar", gender: "male"},
-    {userId : 2, firstName : "Sameer", gender: "male"},
-    {userId : 3, firstName : "Hamza", gender: "male"},
+    { userId: 1, firstName: "Qamar", gender: "male" },
+    { userId: 2, firstName: "Sameer", gender: "male" },
+    { userId: 3, firstName: "Hamza", gender: "male" },
 ]
 
 //forEach array method can take 3 arguments: and return 3 values: one array w.r.t index, index , all arrays of objects
 //parameters custom name hoskty hn order is imp
 
-users.forEach((user, inde, whole_array ) => { 
+users.forEach((user, inde, whole_array) => {
     console.log(user, inde, whole_array);
 })
 console.log("---------------------------")
 
 //map method
-const userNames = users.map((userN) => {
-    return userN.firstName;
+const userNames = users.map((userN, index) => {
+    return [userN.firstName, index];
 })
 console.log(userNames);
+
+const userNamesWithIndex = users.map((userN, index) => {
+    return `${index}: ${userN.firstName}`;
+});
+
+console.log(userNamesWithIndex);
